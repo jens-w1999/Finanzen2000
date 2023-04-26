34757b669a78da25d5fecf1e25c2780df24b47dc
MultiKnacker <nicodahlhaus@outlook.de>
2023-04-26 13:58:32 +0200

    cleanup

--- authentification/venv/Scripts/activate.bat
+++ authentification/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=P:\Studium\w-hs\StudentsLab\Finanzen2000\authentification\venv
+set VIRTUAL_ENV=C:\Studieren\w-hs\slab\Finanzen2000\authentification\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
