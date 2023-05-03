1b0b2eb6dd81e4693f45b5066f71915b7af39c99
Nico <nicodahlhaus@outlook.de>
2023-05-03 12:05:03 +0200

    first example route

--- authentification/authsystem/users/views.py
+++ authentification/authsystem/users/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(requests):
-    return HttpResponse("<h1>Home</h1>")
+def home(request):
+    return render(request,'users/home.html')
