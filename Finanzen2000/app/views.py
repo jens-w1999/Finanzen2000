ac2478b814c809a30553cb6cd3cee160aaef0958
tomtom456 <tom225060@gmail.com>
2023-05-17 10:01:28 +0200

    Routing update

--- Finanzen2000/app/views.py
+++ Finanzen2000/app/views.py
@@ -4,9 +4,29 @@ from app import app
 from flask import render_template
 
 @app.route("/")
-def index():
+def login():
     return render_template("login.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 @app.route("/register")
 def register():
-    return "Hello world"
+    return render_template('register.html')
+
+@app.route("/overview")
+def overview():
+    return render_template('overview.html')
+
+@app.route("/income")
+def income():
+    return render_template('income.html')
+
+@app.route("/cost") 
+def cost():
+    return render_template('cost.html')
+
+@app.route("/assetManagement")
+def assetManagement():
+    return render_template('assetManagement.html')
